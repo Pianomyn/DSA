a9528fd6611c08c3eb216d98ed94e233c9f88e4f
Pianomyn <anditan2014@gmail.com>
2024-01-03 12:55:56 +1100

    save

--- pathfinding/weighted/djikstras.cpp
+++ pathfinding/weighted/djikstras.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cstddef>
 #include <limits.h>
 #include <queue>
@@ -8,13 +7,6 @@
 
 class DjikstasAlgorithm {
   using pqElement = std::pair<int, std::pair<int, int>>;
-
-private:
-  struct SimpleHash {
-    std::size_t operator()(const std::pair<int, int> &p) const {
-      return p.first ^ p.second;
-    }
-  };
 
 public:
   int djikstrasAlgorithm(std::vector<std::vector<int>> &grid) {
@@ -28,33 +20,30 @@ public:
         pq;
     pq.push({0, {0, 0}});
 
-    std::unordered_set<std::pair<int, int>, SimpleHash> visited;
-
     pqElement current;
     int x, y;
-    int cost;
+    int prevCost;
     while (!pq.empty()) {
       current = pq.top();
       pq.pop();
-      cost = current.first;
+      prevCost = current.first;
       x = current.second.first;
       y = current.second.second;
-      visited.insert({x, y});
 
-      if (x - 1 > 0 && visited.find({x - 1, y}) == visited.end()) {
-        dist[x - 1][y] = std::min(dist[x - 1][y], dist[x][y] + grid[x - 1][y]);
+      if (x - 1 > 0 && prevCost + grid[x - 1][y] < dist[x - 1][y]) {
+        dist[x - 1][y] = prevCost + grid[x - 1][y];
         pq.push({dist[x - 1][y], {x - 1, y}});
       }
-      if (x + 1 < n && visited.find({x + 1, y}) == visited.end()) {
-        dist[x + 1][y] = std::min(dist[x + 1][y], dist[x][y] + grid[x + 1][y]);
+      if (x + 1 < n && prevCost + grid[x + 1][y] < dist[x + 1][y]) {
+        dist[x + 1][y] = prevCost + grid[x + 1][y];
         pq.push({dist[x + 1][y], {x + 1, y}});
       }
-      if (y - 1 > 0 && visited.find({x, y - 1}) == visited.end()) {
-        dist[x][y - 1] = std::min(dist[x][y - 1], dist[x][y] + grid[x][y - 1]);
+      if (y - 1 > 0 && prevCost + grid[x][y - 1] < dist[x][y - 1]) {
+        dist[x][y - 1] = prevCost + grid[x][y - 1];
         pq.push({dist[x][y - 1], {x, y - 1}});
       }
-      if (y + 1 < n && visited.find({x, y + 1}) == visited.end()) {
-        dist[x][y + 1] = std::min(dist[x][y + 1], dist[x][y] + grid[x][y + 1]);
+      if (y + 1 < n && prevCost + grid[x][y + 1] < dist[x][y + 1]) {
+        dist[x][y + 1] = prevCost + grid[x][y + 1];
         pq.push({dist[x][y + 1], {x, y + 1}});
       }
     }
