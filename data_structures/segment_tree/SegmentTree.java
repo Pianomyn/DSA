2ba6ccea6999bc7e86d066627981965ced384b3e
Pianomyn <anditan2014@gmail.com>
2025-04-21 23:52:39 +1000

    save

--- data_structures/segment_tree/SegmentTree.java
+++ data_structures/segment_tree/SegmentTree.java
@@ -1,7 +1,7 @@
 // https://www.youtube.com/watch?v=Tr-xEGoByFQ&list=PL1gVuOYe08PT54itbrJmJbmLrk1W1DXxu&index=5
 
 interface RangeQuery {
-  int minimum(int start, int end);
+  int minimum(int i, int start, int end);
 }
 interface RangeUpdate {
 }
@@ -12,37 +12,99 @@ interface PointUpdate {
 
 class SegmentTree implements RangeQuery {
   int n;
-  int[] rangeLow,  // For a point, the low end of the range its responsible for.
-        rangeHigh, // For a point, the high end of the range its responsible for.
+  int[] rangeLow,  // For a point, the low end of the range it's responsible for.
+        rangeHigh, // For a point, the high end of the range it's responsible for.
         min,  //
         max,  //
         sum,  //
-        delta;  //
+        delta;  //  For lazy propagation
 
+  /* If n is a power of 2, only need 2n - 1 nodes (n leaves, n-1 internal).
+   * For safety, pad to the next power of 2 to accommodate all nodes.
+   */
   public SegmentTree(int n) {
     this.n = n;
-    // If n is a power of 2, only need 2n - 1 nodes (n leaves, n-1 internal).
-    // For safety, pad to the next power of 2 to accommodate all nodes.
     this.rangeLow = new int[4 * n];
     this.rangeHigh = new int[4 * n];
+
+    initRange(1, 0, n-1);  // 1-indexing
   }
 
-  void initRange(int nodeIndex, int low, int high) {
-    this.rangeLow[nodeIndex] = low;
-    this.rangeHigh[nodeIndex] = high;
+  void initRange(int i, intlow, int high) {
+    this.rangeLow[i] = low;
+    this.rangeHigh[i] = high;
 
     if (low == high) {
       return;
     }
 
     int mid = low + (high - low) / 2;
-    initRange(2*nodeIndex, low, mid);  // Left
-    initRange(2 * nodeIndex, mid+1, high);  // Right
+    initRange(2*i, low, mid);  // Left
+    initRange(2 * i, mid+1, high);  // Right
+  }
 
+  void propagate(int i) {
+    this.delta[2 * i] += this.delta[i];
+    this.delta[2 * i + 1] += this.delta[i];
+    this.delta[i] = 0;
+  }
+
+  void rangeUpdate(int i, int start, int end, int change) {
+    // Uncovered
+    if(start < this.rangeLow[i] || this.rangeHigh[i] < end) {
+      return;
+    }
+
+    // Fully covered
+    if(start <= this.rangeLow[i] && this.rangeHigh[i] <= end) {
+      this.delta[i] += change;
+      return;
+    }
+
+    // Partially covered
+    propagate(i);
+
+    rangeUpdate(2 * i, start, end, change);
+    rangeUpdate(2 * i + 1, start, end, change);
+  }
+
+  /* --------------------- MINIMUM ---------------------*/
+  void updateMin(int i) {
+    this.min[i] = Math.min(
+      this.min[2 * i] + this.delta[2 * i],
+      this.min[2 * i + 1] + this.delta[2 * i + 1]
+    );
+  }
+
+  void rangeUpdateMin(int i, int start, int end, int change) {
+    // Down
+    rangeUpdate(i, start, end, change);
+    // Up
+    updateMin(i);
   }
 
   @Override
-  public int minimum(int start, int end) {
-    return 0;
+  public int minimum(int i, int start, int end) {
+    if(end < this.rangeLow[i] || this.rangeHigh[i] < start) {
+      return Integer.MAX_VALUE;
+    }
+
+    if(start <= this.rangeLow[i] && this.rangeHigh[i] <= end) {
+      return this.min[i] + delta[i];
+    }
+
+    propagate(i);
+
+    int minLeft = minimum(2 * i, start, end);
+    int minRight = minimum(2 * i + 1, start, end);
+
+    updateMin(i);
+
+    return Math.min(minLeft, minRight);
   }
+
+  /* --------------------- MAXIMUM ---------------------*/
+
+  /* --------------------- SUM ---------------------*/
+
 }
