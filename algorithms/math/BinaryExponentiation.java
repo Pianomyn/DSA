17f31aa4712b7b0974afeb8604a1dd27b4662d87
Pianomyn <anditan2014@gmail.com>
2025-04-22 15:37:22 +1000

    save

---
+++ algorithms/math/BinaryExponentiation.java
@@ -0,0 +1,29 @@
+class BinaryExponentiation {
+  // Recursive
+  public static long power(int base, int exponent) {
+    if (exponent == 0) {
+      return 1L;
+    }
+
+    long half = power(base, exponent / 2);
+
+    if (exponent % 2 == 1) {
+      return base * half * half;
+    }
+    return half * half;
+  }
+
+  long powerIterative(long base, long exponent) {
+    long res = 1;
+
+    while (exponent > 0) {
+      if (exponent % 2 == 1) {
+        res = res * base;
+      }
+      base = base * base;
+      exponent /= 2;
+    }
+
+    return res;
+  }
+}
