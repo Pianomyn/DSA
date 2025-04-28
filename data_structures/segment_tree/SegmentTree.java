867e9dd6927a1582c2c2b80a2142463858969056
Pianomyn <anditan2014@gmail.com>
2025-04-28 19:11:20 +1000

    save

--- data_structures/segment_tree/SegmentTree.java
+++ data_structures/segment_tree/SegmentTree.java
@@ -158,11 +158,19 @@ class SegmentTreeTest {
     FakeSegmentTree fst = new FakeSegmentTree(arr);
 
     assert st.rangeMinimum(1, 0, 5) == fst.rangeMinimum(1, 0, 5);
+    assert st.rangeMinimum(1, 1, 3) == fst.rangeMinimum(1, 0, 5);
 
     st.rangeUpdate(1, 0, 0, -22, UpdateType.MIN);
     fst.rangeUpdate(1, 0, 0, -22, UpdateType.MIN);
 
     assert st.rangeMinimum(1, 0, 5) == fst.rangeMinimum(1, 0, 5);
     assert st.rangeMinimum(1, 1, 5) == fst.rangeMinimum(1, 1, 5);
+
+    st.rangeUpdate(1, 5, 5, -22, UpdateType.MIN);
+    fst.rangeUpdate(1, 5, 5, -22, UpdateType.MIN);
+
+    assert st.rangeMinimum(1, 0, 5) == fst.rangeMinimum(1, 0, 5);
+    assert st.rangeMinimum(1, 0, 4) == fst.rangeMinimum(1, 0, 5);
+    assert st.rangeMinimum(1, 1, 3) == fst.rangeMinimum(1, 0, 5);
   }
 }
